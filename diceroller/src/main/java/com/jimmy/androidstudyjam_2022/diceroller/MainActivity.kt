c857843e86bbbd7f5c66a63680848a41be831a93
Jimmy90321 <jimmy4302001@gmail.com>
2022-06-08 22:29:22 +0800

    Add a button 2

---
+++ diceroller/src/main/java/com/jimmy/androidstudyjam_2022/diceroller/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.jimmy.androidstudyjam_2022.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val sides: Int) {
+    fun roll(): Int = (1..sides).random()
+}
